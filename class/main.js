3ee35ad5b7aea96f60a7bc540e14be9ae32b3732
Scott Feichter <scottfeichter@gmail.com>
2024-02-02 14:09:47 -0800

    corrected som elogice. stuck on Bicycle not defined. Don't udnerstand why. Toggled imports no luck.

---
+++ class/main.js
@@ -0,0 +1,6 @@
+const Bicycle = require(`./bicycle`);
+
+
+let validBicycle = new Bicycle("Trek 520", 2023, 1829);
+
+module.exports = validBicycle;
--- class/tester.js
+++ class/tester.js
@@ -1,4 +1,8 @@
+const Vehicle = require(`./vehicle`);
+// const Bicycle = require(`./bicycle`);
+const ElectricBicycle = require("./electric-bicycle");
 const Review = require(`./review`);
+const validBicycle = require(`./main`);
 
 class Tester {
   constructor(name, reviews = []) {
@@ -16,6 +20,22 @@ class Tester {
     return nuReview;
   }
 
+  _isVehicleEBicycle(vehicle) {
+    console.log("_isVehicleEBicyle: Running");
+    if (vehicle instanceof ElectricBicycle) {
+      return true;
+    }
+    return false;
+  }
+
+  _isVehicleBicycle(vehicle) {
+    console.log("_isVehicleBicyle: Running");
+    if (vehicle instanceof Bicyle) {
+      return true;
+    }
+    return false;
+  }
+
   _addReview(nuReview) {
     nuReview.addReview();
   }
@@ -28,14 +48,47 @@ class Tester {
       reviewedText
     );
 
-    if (reviewedVehicle.validate()) {
+    if (
+      reviewedVehicle instanceof Vehicle &&
+      !this._isVehicleEBicycle(reviewedVehicle) &&
+      !this._isVehicleBicycle(reviewedVehicle)
+    ) {
+      if (reviewedVehicle.validate()) {
+        nuReview.addReview();
+        return nuReview;
+      } else {
+        throw new Error("Cannot submit review for invalid vehicle.");
+      }
+    }
+
+    if (this._isVehicleEBicycle(reviewedVehicle)) {
+      console.log("Yes EBike");
       nuReview.addReview();
-    } else {
-      throw new Error("Cannot submit review for invalid vehicle.");
+      this.tester["bikeTester"] = true;
+      this.tester.eBiketester = true;
+      return nuReview;
+    } else if (this._isVehicleBicycle(reviewedVehicle)) {
+      console.log("Yes Bike");
+      nuReview.addReview();
+      this.tester["bikeTester"] = true;
+      return nuReview;
     }
 
     return nuReview;
   }
 }
+
+let tester = new Tester("Bob Jones");
+
+console.log(
+  tester.submitReview(validBicycle, 5, "Great for touring and gravel")
+);
+console.log(tester.bikeTester); // true
+console.log(tester.eBikeTester); // undefined or false
+
+// let validEBike = new ElectricBicycle("Specialized", 2010, 600, "Touring", 18, 26, 30, 45);
+// tester.submitReview(validEBike, 1, "Terrible range");
+// console.log(tester.bikeTester) // true
+// console.log(tester.eBikeTester) // true
 
 module.exports = Tester;
