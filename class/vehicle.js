2bb1acbba04bc80afc7c2b6f1798d39132f8735f
Scott Feichter <scottfeichter@gmail.com>
2024-02-02 08:59:46 -0800

    updating and handling errors finsished. 13 passing

--- class/vehicle.js
+++ class/vehicle.js
@@ -7,14 +7,21 @@ class Vehicle {
   }
 
   _isValidYear(year) {
-    if (this.year < 1950 || this.year > 2100) {
+    if (year < 1950 || year > 2100) {
       return false;
     } else {
       return true;
     }
   }
 
-  _isValidMeta() {
+  _isValidPrice(price) {
+    if (price > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  _isPresentMeta() {
     if (this.modelName && this.year && this.price) {
       return true;
     } else {
@@ -23,13 +30,56 @@ class Vehicle {
   }
 
   validate() {
-    if (this._isValidYear(this.year) && this._isValidMeta()) {
+    if (
+      this._isValidYear(this.year) &&
+      this._isValidPrice(this.price) &&
+      this._isPresentMeta()
+    ) {
       return true;
     }
     return false;
   }
 
-  update(modelName, year, price) {}
+  _updateModelName(nuModelName) {
+    if (nuModelName) {
+      this.modelName = nuModelName;
+      return true;
+    }
+    return false;
+  }
+
+  _updateYear(nuYear) {
+    if (this._isValidYear(nuYear)) {
+      this.year = nuYear;
+      return true;
+    } else {
+      throw new Error("Year must be between 1950 and 2100");
+    }
+  }
+
+  _updatePrice(nuPrice) {
+    if (nuPrice) {
+      if (this._isValidPrice) {
+        this.price = nuPrice;
+        return true;
+      }
+    } else {
+      throw new Error("Price must be greater than 0");
+    }
+  }
+
+  update(nuModelName, nuYear, nuPrice) {
+    if (
+      this._updateModelName(nuModelName) &&
+      this._updateYear(nuYear) &&
+      this._updatePrice(nuPrice)
+    ) {
+      return true;
+    } else {
+      console.error("Did not update - check errors!");
+      return false;
+    }
+  }
 }
 
 module.exports = Vehicle;
