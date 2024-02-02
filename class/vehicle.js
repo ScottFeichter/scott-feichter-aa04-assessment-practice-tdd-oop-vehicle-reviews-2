a26f894858c805624dc301f8c395025f749741ae
Scott Feichter <scottfeichter@gmail.com>
2024-02-02 08:34:42 -0800

    validating data finished. 10 passing

--- class/vehicle.js
+++ class/vehicle.js
@@ -5,6 +5,15 @@ class Vehicle {
     this.price = price;
     this.reviews = reviews;
   }
+  validate() {
+    if (this.year < 1950 || this.year > 2100) {
+      return false;
+    } else if (!(this.modelName) || !(this.year) || !(this.price)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 module.exports = Vehicle;
